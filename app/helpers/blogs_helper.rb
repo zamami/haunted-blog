6b296e3c095755ef88d29185afa775a48ad5e924
zamami <s1413115@gmail.com>
2025-04-14 18:02:48 +0900

    ２重にエスケープ処理を修正、改行タグのみ使えるようにしました。

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,11 +2,6 @@
 
 module BlogsHelper
   def format_content(blog)
-    tags = %w[a acronym b strong i em li ul ol h1 h2 h3 h4 h5 h6 blockquote br cite sub sup ins p]
-    sanitize(
-      h(blog.content).gsub("\n", '<br>'),
-      tags:,
-      attributes: %w[href title]
-    )
+    safe_join(blog.content.split("\n"), tag.br)
   end
 end
